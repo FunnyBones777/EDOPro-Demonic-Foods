05372103837c3c12a516e81fce31dee99449969c
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2021-08-15 12:37:23 -0400

    1 new card added and fixed all the archetypes

---
+++ script/c66666018.lua
@@ -0,0 +1,54 @@
+-- Omega Mart
+local s, id = GetID()
+function s.initial_effect(c)
+  --Activate
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_ACTIVATE)
+  e1:SetCode(EVENT_FREE_CHAIN)
+  c:RegisterEffect(e1)
+  --atkup
+  local e2=Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_FIELD)
+  e2:SetCode(EFFECT_UPDATE_ATTACK)
+  e2:SetRange(LOCATION_FZONE)
+  e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+  e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x18b))
+  e2:SetValue(2020)
+  c:RegisterEffect(e2)
+  --special summon
+  local e3=Effect.CreateEffect(c)
+  e3:SetDescription(aux.Stringid(id,0))
+  e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+  e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+  e3:SetRange(LOCATION_FZONE)
+  e3:SetCode(EVENT_TO_GRAVE)
+  e3:SetCountLimit(1)
+  e3:SetCondition(s.condition)
+  e3:SetTarget(s.target)
+  e3:SetOperation(s.operation)
+  c:RegisterEffect(e3)
+end
+
+function s.filter(c,e,tp,lv)
+  return c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.cfilter(c,tp)
+	return c:IsReason(REASON_DESTROY) and c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp) and (c:IsSetCard(0x18a) or c:IsSetCard(0x18b))
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
