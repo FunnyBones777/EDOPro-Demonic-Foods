feb597f6565ca5dcb9f8d82b2330bea0b7c51370
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2021-08-18 16:35:26 -0400

    Changed the effect of "Dark Ingredient Giant's Honey"

--- script/c66666024.lua
+++ script/c66666024.lua
@@ -15,9 +15,9 @@ function s.initial_effect(c)
   e1:SetOperation(s.activate)
   c:RegisterEffect(e1)
 end
-s.listed_series={0x18a}
+s.listed_series={0x18b}
 function s.filter(c,e,tp)
-    return c:IsSetCard(0x18a) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+    return c:IsSetCard(0x18b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
