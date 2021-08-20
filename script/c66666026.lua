e31e2bcd3360fe1138357a6c874759693032b910
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2021-08-20 00:48:29 -0400

    Updated "Demonic Milkshake Kasib Colada" (Fifth Time)

--- script/c66666026.lua
+++ script/c66666026.lua
@@ -1,34 +1,67 @@
 -- Demonic Milkshake Kasib Colada
 local s, id = GetID()
 function s.initial_effect(c)
-  --synchro summon
-  c:EnableReviveLimit()
-  Synchro.AddMajesticProcedure(c,aux.FilterBoolFunction(Card.IsCode,66666023),true,aux.FilterBoolFunction(Card.IsCode,66666013),true,Synchro.NonTunerEx(Card.IsRace,RACE_ZOMBIE),1,99)
+  --pendulum summon
+  Pendulum.AddProcedure(c)
+  --Add from the deck to the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+  e1:SetCountLimit(1,id)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
   --race
-  local e1=Effect.CreateEffect(c)
-  e1:SetType(EFFECT_TYPE_FIELD)
-  e1:SetRange(LOCATION_MZONE)
-  e1:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE)
-  e1:SetCode(EFFECT_CHANGE_RACE)
-  e1:SetValue(RACE_ZOMBIE)
-  e1:SetTarget(s.tg)
-  c:RegisterEffect(e1)
-  --damage
   local e2=Effect.CreateEffect(c)
-  e2:SetDescription(aux.Stringid(id,0))
-  e2:SetCategory(CATEGORY_DAMAGE)
-  e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-  e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+  e2:SetType(EFFECT_TYPE_FIELD)
   e2:SetRange(LOCATION_MZONE)
-  e2:SetCountLimit(1)
-  e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-  e2:SetCondition(s.damcon)
-  e2:SetTarget(s.damtg)
-  e2:SetOperation(s.damop)
+  e2:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE)
+  e2:SetCode(EFFECT_CHANGE_RACE)
+  e2:SetValue(RACE_ZOMBIE)
+  e2:SetTarget(s.tg)
   c:RegisterEffect(e2)
+  --damage
+  local e3=Effect.CreateEffect(c)
+  e3:SetDescription(aux.Stringid(id,0))
+  e3:SetCategory(CATEGORY_DAMAGE)
+  e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+  e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+  e3:SetRange(LOCATION_MZONE)
+  e3:SetCountLimit(1)
+  e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+  e3:SetCondition(s.damcon)
+  e3:SetTarget(s.damtg)
+  e3:SetOperation(s.damop)
+  c:RegisterEffect(e3)
 end
-s.material={66666023,66666013}
-s.listed_names={66666023,66666013}
+s.listed_series={0x18a}
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.filter(c)
+	return c:IsSetCard(0x18a) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDestructable()
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) and Duel.GetFlagEffect(tp,id+1)==0 end
+	Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,1)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or Duel.Destroy(c,REASON_EFFECT)==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
 function s.tg(e,c)
   if c:GetFlagEffect(1)==0 then
     c:RegisterFlagEffect(1,0,0,0)
@@ -55,7 +88,7 @@ end
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
   if chk==0 then return true end
   Duel.SetTargetPlayer(1-tp)
-  Duel.SetTargetParam(800)
+  Duel.SetTargetParam(1000)
   Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,800)
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
