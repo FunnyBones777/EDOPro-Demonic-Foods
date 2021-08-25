79cf31fc57bea04d677383e6fd475282680eb59a
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2021-08-25 09:55:34 -0400

    Introducing not "Infinite Inferno" monsters but the "Demon Chef" monsters instead

--- script/c66666019.lua
+++ script/c66666019.lua
@@ -1,7 +1,7 @@
 -- Desiring Fusion
 local s,id=GetID()
 function s.initial_effect(c)
-    local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x18c),nil,s.fextra)
+    local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x18c),nil,nil)
     c:RegisterEffect(e1)
     if not AshBlossomTable then AshBlossomTable={} end
     table.insert(AshBlossomTable,e1)
---
+++ script/c66666029.lua
@@ -0,0 +1,51 @@
+-- Demon Chef Matt
+local s, id = GetID()
+function s.initial_effect(c)
+  --pendulum summon
+  Pendulum.AddProcedure(c)
+  --scale
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_LSCALE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(s.slcon)
+	e1:SetValue(5)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CHANGE_RSCALE)
+	c:RegisterEffect(e2)
+  --change race
+  local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_ACTIVATE)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(s.racetg)
+	e3:SetOperation(s.raceop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x18d}
+function s.slfilter(c)
+	return c:IsSetCard(0x18d)
+end
+function s.slcon(e)
+	return not Duel.IsExistingMatchingCard(s.slfilter,e:GetHandlerPlayer(),LOCATION_PZONE,0,1,e:GetHandler())
+end
+function s.racetg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local aat=e:GetHandler():AnnounceAnotherRace(tp)
+	e:SetLabel(aat)
+end
+function s.raceop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_RACE)
+		e1:SetValue(e:GetLabel())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
