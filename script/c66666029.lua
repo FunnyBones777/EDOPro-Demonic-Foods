b433488fb57324275588f1988576ac205cf0515d
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2021-08-25 10:02:01 -0400

    Fixed the "Demon Chef" monsters

--- script/c66666029.lua
+++ script/c66666029.lua
@@ -18,7 +18,7 @@ function s.initial_effect(c)
   --change race
   local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetType(EFFECT_TYPE_ACTIVATE)
+	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
